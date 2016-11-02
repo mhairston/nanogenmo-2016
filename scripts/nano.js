ce4247cde19cbf85bf3b92d2fed62eeca571e914
Michael Hairston <mhairston@vistahigherlearning.com>
2016-11-02 07:49:14 -0400

    First version. Broken but informative.

---
+++ scripts/nano.js
@@ -0,0 +1,30 @@
+var sep = '`';
+
+function rr(min, max) {
+  if (!max) {
+    max = min;
+    min = 0;
+  }
+  return Math.floor(Math.random()*(max - min) + min);
+}
+
+function choose(collection, space) {
+  var after = (space) ? ' ' : '';
+  return collection[rr(collection.length)] + after;
+}
+
+function resolveNode(node) {
+  var alternatives = node.innerHTML;
+  if (alternatives.length > 1) {
+    node
+      .html(choose(alternatives.split(sep)))
+      .addClass('is-done');
+  }
+}
+
+function writeNovel(novel) {
+  //$('q').each(resolveNode);
+}
+
+var novel = document.querySelector('.c-main');
+writeNovel(novel);
